285bcbfc3842b07fe8657df34a8f7eebb1c061f7
evgeny-pol <jekapolet777@gamil.com>
2015-05-06 23:39:16 +0300

    controller created take 2 (untracked files)

---
+++ WorkTimer/WorkTimer/WorkTimerController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace WorkTimer
+{
+    /// <summary>
+    /// Class is NOT thread safe
+    /// </summary>
+    public class WorkTimerController
+    {
+        private readonly TimeSpan _maxWorkPeriodDuration;
+
+        /// <summary>
+        /// Value that indicates when current work period started
+        /// NULL value means that there is no work currently underway
+        /// </summary>
+        private DateTime? _currentWorkPeriodStart;
+
+        public WorkTimerController(TimeSpan workDurationBeforeBreak)
+        {
+            _maxWorkPeriodDuration = workDurationBeforeBreak;
+        }
+
+        public void WorkPeriodStarted()
+        {
+            _currentWorkPeriodStart = DateTime.Now;
+        }
+
+        public void WorkPeriodEnded()
+        {
+            _currentWorkPeriodStart = null;
+        }
+    }
+}
