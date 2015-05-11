10a9fc3d010e934b1f3ee71ffa0eff584448b92d
evgeny-pol <jekapolet777@gamil.com>
2015-05-12 00:15:38 +0300

    session switch handler added

--- WorkTimer/WorkTimer/MainWindow.xaml.cs
+++ WorkTimer/WorkTimer/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using Microsoft.Win32;
+using System.Windows;
 using WorkTimer.Properties;
 
 namespace WorkTimer
@@ -12,11 +13,52 @@ namespace WorkTimer
             InitializeComponent();
 
             _controller = new WorkTimerController(Settings.Default.MaxWorkDurationBeforeBreak);
+
+            SystemEvents.SessionSwitch += HandleSystemSessionSwitch;
         }
 
-        private void OnLoaded(object sender, RoutedEventArgs e)
+        private void OnLoaded(object sender, RoutedEventArgs eventArgs)
+        {
+            WorkStarted();
+        }
+
+        private void HandleSystemSessionSwitch(object sender, SessionSwitchEventArgs eventArgs)
+        {
+            /*
+             * SessionSwitchReason:
+             * ConsoleConnect       - session has been connected from the console.
+             * ConsoleDisconnect    - session has been disconnected from the console.
+             * RemoteConnect        - session has been connected from a remote connection.
+             * RemoteDisconnect     - session has been disconnected from a remote connection.
+             * SessionLock          - session has been locked.
+             * SessionLogoff        - user has logged off from a session.
+             * SessionLogon         - user has logged on to a session.
+             * SessionRemoteControl - session has changed its status to or from remote controlled mode.
+             * SessionUnlock        - session has been unlocked.
+             */
+
+            // todo: test in multi-user environment
+
+            switch (eventArgs.Reason)
+            {
+                case SessionSwitchReason.SessionUnlock:
+                    WorkStarted();
+                    break;
+
+                case SessionSwitchReason.SessionLock:
+                    WorkSuspended();
+                    break;
+            }
+        }
+
+        private void WorkStarted()
         {
             _controller.WorkPeriodStarted();
+        }
+
+        private void WorkSuspended()
+        {
+            _controller.WorkPeriodEnded();
         }
     }
 }
