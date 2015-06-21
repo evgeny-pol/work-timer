1a92a50928ed481b7ec9fd291ee3513bc46483a0
evgeny-pol <jekapolet777@gamil.com>
2015-06-21 21:54:29 +0300

    BreakRequired event added

---
+++ WorkTimer/WorkTimer/BreakRequiredEventArgs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace WorkTimer
+{
+    public sealed class BreakRequiredEventArgs
+    {
+        public TimeSpan CurrentWorkPeriodDuration { get; private set; }
+
+        public BreakRequiredEventArgs(TimeSpan currentWorkPeriodDuration)
+        {
+            CurrentWorkPeriodDuration = currentWorkPeriodDuration;
+        }
+    }
+}
--- WorkTimer/WorkTimer/WorkTimerController.cs
+++ WorkTimer/WorkTimer/WorkTimerController.cs
@@ -1,13 +1,19 @@
 ﻿using System;
+using System.Threading;
 
 namespace WorkTimer
 {
     /// <summary>
     /// Class is NOT thread safe
     /// </summary>
-    public class WorkTimerController
+    public sealed class WorkTimerController
     {
+        private const double TimerPeriodMinutes = 1;
+
+        public event Action<WorkTimerController, BreakRequiredEventArgs> BreakRequired;
+
         private readonly TimeSpan _maxWorkPeriodDuration;
+        private readonly Timer _timer;
 
         /// <summary>
         /// Value that indicates when current work period started
@@ -18,6 +24,11 @@ namespace WorkTimer
         public WorkTimerController(TimeSpan workDurationBeforeBreak)
         {
             _maxWorkPeriodDuration = workDurationBeforeBreak;
+
+            _timer = new Timer(TimerCallback,
+                               null,
+                               TimeSpan.FromMilliseconds(0),
+                               TimeSpan.FromMinutes(TimerPeriodMinutes));
         }
 
         public void WorkPeriodStarted()
@@ -28,6 +39,23 @@ namespace WorkTimer
         public void WorkPeriodEnded()
         {
             _currentWorkPeriodStart = null;
+        }
+
+        private void TimerCallback(object state)
+        {
+            if (_currentWorkPeriodStart == null)
+            {
+                return;
+            }
+
+            var now = DateTime.Now;
+            var currentWorkPeriodDuration = now - _currentWorkPeriodStart;
+
+            if (currentWorkPeriodDuration > _maxWorkPeriodDuration)
+            {
+                var eventArgs = new BreakRequiredEventArgs(currentWorkPeriodDuration.Value);
+                BreakRequired.Call(this, eventArgs);
+            }
         }
     }
 }
