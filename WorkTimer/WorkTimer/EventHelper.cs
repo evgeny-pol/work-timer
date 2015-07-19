9383adf586750eb0b2e175f2eb1d0f7a88546549
evgeny-pol <jekapolet777@gamil.com>
2015-07-19 17:22:10 +0300

    EventHelper class

---
+++ WorkTimer/WorkTimer/EventHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading;
+
+namespace WorkTimer
+{
+    public static class EventHelper
+    {
+        public static void Call<T1, T2>(this Action<T1, T2> @event, T1 arg1, T2 arg2)
+        {
+            var eventValue = Interlocked.CompareExchange(ref @event, null, null);
+
+            if (eventValue != null)
+            {
+                eventValue(arg1, arg2);
+            }
+        }
+    }
+}
